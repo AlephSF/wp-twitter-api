6b6672a1ba8c2aadf0c70fdbf114089517830515
Matt Glaser <matt@jmpdv.com>
2016-11-08 12:52:33 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
  * Plugin Name: Essential Product Tweets
  */
 if( ! function_exists('twitter_api_get') ){
-    require dirname(__FILE__).'/api/twitter-api.php';
+    require dirname(__FILE__).'/twitter-api.php';
 }
